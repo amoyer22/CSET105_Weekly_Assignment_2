efec94601ad2fab3772ff3062a3d371772fcad6c
Ailish <snickrrdoodles@gmail.com>
2024-09-30 13:05:05 -0400

    Player's choices show up on document properly

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-function gameStart(){
+function playerChoice(choice){
     let computerRNG = Math.floor(Math.random() * 3) + 1;
+
+    if(choice === 'rock'){
+        document.getElementById("playerChoice").innerHTML = "Player's Choice: Rock"
+    }
+    else if(choice === 'paper'){
+        document.getElementById("playerChoice").innerHTML = "Player's Choice: Paper"
+    }
+    else if(choice === 'scissors'){
+        document.getElementById("playerChoice").innerHTML = "Player's Choice: Paper"
+    }
 
     if(computerRNG === 1){
         document.getElementById("computerChoice").innerHTML = "Computer's Choice: Rock"
     }
-    if(computerRNG === 2){
+    else if(computerRNG === 2){
         document.getElementById("computerChoice").innerHTML = "Computer's Choice: Paper"
     }
-    if(computerRNG === 3){
+    else if(computerRNG === 3){
         document.getElementById("computerChoice").innerHTML = "Computer's Choice: Scissors"
     }
 }
