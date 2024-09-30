7233d61c4dc95f17d4481b02f08f03024b9ec316
Ailish <snickrrdoodles@gmail.com>
2024-09-30 13:42:27 -0400

    Set up possible outcomes for result span

--- script.js
+++ script.js
@@ -2,22 +2,48 @@ function playerChoice(choice){
     let computerRNG = Math.floor(Math.random() * 3) + 1;
 
     if(choice === 'rock'){
-        document.getElementById("playerChoice").innerHTML = "Player's Choice: Rock"
+        document.getElementById("playerChoice").innerHTML = "Rock"
     }
     else if(choice === 'paper'){
-        document.getElementById("playerChoice").innerHTML = "Player's Choice: Paper"
+        document.getElementById("playerChoice").innerHTML = "Paper"
     }
     else if(choice === 'scissors'){
-        document.getElementById("playerChoice").innerHTML = "Player's Choice: Paper"
+        document.getElementById("playerChoice").innerHTML = "Scissors"
     }
 
+
     if(computerRNG === 1){
-        document.getElementById("computerChoice").innerHTML = "Computer's Choice: Rock"
+        document.getElementById("computerChoice").innerHTML = "Rock"
     }
     else if(computerRNG === 2){
-        document.getElementById("computerChoice").innerHTML = "Computer's Choice: Paper"
+        document.getElementById("computerChoice").innerHTML = "Paper"
     }
     else if(computerRNG === 3){
-        document.getElementById("computerChoice").innerHTML = "Computer's Choice: Scissors"
+        document.getElementById("computerChoice").innerHTML = "Scissors"
+    }
+
+
+    if(document.getElementById("playerChoice").innerHTML === document.getElementById("computerChoice").innerHTML){
+        document.getElementById("result").innerHTML = "Tie!"
+    }
+
+    else if(document.getElementById("playerChoice").innerHTML == "Rock" && document.getElementById("computerChoice").innerHTML == "Scissors"){
+        document.getElementById("result").innerHTML = "Player wins!"
+    }
+    else if(document.getElementById("playerChoice").innerHTML == "Paper" && document.getElementById("computerChoice").innerHTML == "Rock"){
+        document.getElementById("result").innerHTML = "Player wins!"
+    }
+    else if(document.getElementById("playerChoice").innerHTML == "Scissors" && document.getElementById("computerChoice").innerHTML == "Paper"){
+        document.getElementById("result").innerHTML = "Player wins!"
+    }
+
+    else if(document.getElementById("playerChoice").innerHTML == "Rock" && document.getElementById("computerChoice").innerHTML == "Paper"){
+        document.getElementById("result").innerHTML = "Computer wins!"
+    }
+    else if(document.getElementById("playerChoice").innerHTML == "Paper" && document.getElementById("computerChoice").innerHTML == "Scissors"){
+        document.getElementById("result").innerHTML = "Computer wins!"
+    }
+    else if(document.getElementById("playerChoice").innerHTML == "Scissors" && document.getElementById("computerChoice").innerHTML == "Rock"){
+        document.getElementById("result").innerHTML = "Computer wins!"
     }
 }
