59e3acd32177555e1df3ef99ba417920dcedf689
Ailish <snickrrdoodles@gmail.com>
2024-09-30 12:54:53 -0400

    Set up random choices for computer

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-let playerScore = 0
-let computerScore = 0
+function gameStart(){
+    let computerRNG = Math.floor(Math.random() * 3) + 1;
+
+    if(computerRNG === 1){
+        document.getElementById("computerChoice").innerHTML = "Computer's Choice: Rock"
+    }
+    if(computerRNG === 2){
+        document.getElementById("computerChoice").innerHTML = "Computer's Choice: Paper"
+    }
+    if(computerRNG === 3){
+        document.getElementById("computerChoice").innerHTML = "Computer's Choice: Scissors"
+    }
+}
